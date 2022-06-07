8804ea410744ac22c4649b7c31c20b0e35e46ad8
Amorena Lucas <103912718+amorenalucas@users.noreply.github.com>
2022-06-06 23:09:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function infousuario(cliente,producto,cantidad,medida){
+    console.log("Cliente: " + cliente)
+    console.log("Producto: " + producto)
+    console.log("Cantidad: " + cantidad)
+    console.log("Medida: " + medida)
+}
+
+infousuario("Amorena", "float 4mm", 1, "1000mm x 1800mm")
